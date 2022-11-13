7436a7027c5065daf8d38804f754fd6bb2f8755e
saurav <sauravkumaryadav100@gmail.com>
2022-11-13 16:24:28 +0530

    added background image

--- frontend.py
+++ frontend.py
@@ -2,6 +2,29 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+
+import base64
+
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+        f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
+
+
+add_bg_from_local("bg.jpg")
+
+
 teams = [
     "Sunrisers Hyderabad",
     "Mumbai Indians",
@@ -46,27 +69,27 @@ cities = [
 ]
 
 pipe = pickle.load(open("pipe.pkl", "rb"))
-st.title("IPL Win Predictor")
+st.title("IPL cricket match outcome predictor")
 
-col1, col2 = st.beta_columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
-    batting_team = st.selectbox("Select the batting team", sorted(teams))
+    batting_team = st.selectbox("Select team on  crease", sorted(teams))
 with col2:
-    bowling_team = st.selectbox("Select the bowling team", sorted(teams))
+    bowling_team = st.selectbox("Select fielding team", sorted(teams))
 
-selected_city = st.selectbox("Select host city", sorted(cities))
+selected_city = st.selectbox("Match venue", sorted(cities))
 
 target = st.number_input("Target")
 
-col3, col4, col5 = st.beta_columns(3)
+col3, col4, col5 = st.columns(3)
 
 with col3:
-    score = st.number_input("Score")
+    score = st.number_input("Current Score")
 with col4:
-    overs = st.number_input("Overs completed")
+    overs = st.number_input("Overs Bowled")
 with col5:
-    wickets = st.number_input("Wickets out")
+    wickets = st.number_input("Wickets taken")
 
 if st.button("Predict Probability"):
     runs_left = target - score
